a34aafb713a40e7237ff831887653670cce1af93
Alan Galavis <alangalavisd@gmail.com>
2025-09-16 18:00:12 -0500

    fix: Issue with CORS and HTTPS

--- src/main/java/com/tp1202510030/backend/companies/interfaces/rest/resources/company/CreateCompanyResource.java
+++ src/main/java/com/tp1202510030/backend/companies/interfaces/rest/resources/company/CreateCompanyResource.java
@@ -4,4 +4,15 @@ public record CreateCompanyResource(
         String companyName,
         Long taxIdentificationNumber
 ) {
+    public CreateCompanyResource {
+        if (companyName == null || companyName.isBlank()) {
+            throw new IllegalArgumentException("companyName cannot be null or empty");
+        }
+        if (taxIdentificationNumber == null) {
+            throw new IllegalArgumentException("taxIdentificationNumber cannot be null or empty");
+        }
+        if (taxIdentificationNumber.toString().length() != 11) {
+            throw new IllegalArgumentException("taxIdentificationNumber must be 11 digits");
+        }
+    }
 }
--- src/main/java/com/tp1202510030/backend/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
+++ src/main/java/com/tp1202510030/backend/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
@@ -4,23 +4,29 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfiguration {
+
+    @Value("${server.url}")
+    private String serverUrl;
+
     @Bean
     public OpenAPI greenhouseOpenApi() {
         final String securitySchemeName = "bearerAuth";
 
         return new OpenAPI()
+                .addServersItem(new Server().url(serverUrl))
                 .info(new Info()
                         .title("Greenhouse API")
                         .description("Greenhouse application REST API documentation.")
                         .version("v1.0.0")
                         .contact(new io.swagger.v3.oas.models.info.Contact()
                                 .name("alanegd")))
-                //.addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
                 .components(new Components()
                         .addSecuritySchemes(securitySchemeName,
                                 new SecurityScheme()
